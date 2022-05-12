e2d35e57055de599be27c1149079ac23f4d655bb
Mspring18 <103467617+Mspring18@users.noreply.github.com>
2022-05-12 10:11:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/es/integration/react";
+import { PersistGate } from "redux-persist/lib";
 import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
